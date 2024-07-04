04db1fde8e0bb6d8780f7bfce7da1f860cda481a
Lucas <lucasvonelm@gmail.com>
2024-07-03 22:22:28 -0300

    Loading animation working on new search

--- frontend/src/hooks/useSearchMovies.js
+++ frontend/src/hooks/useSearchMovies.js
@@ -1,10 +1,16 @@
-import { useState } from 'react';
-import movieService from '../services/moviesService';
+import { useState } from "react";
+import movieService from "../services/moviesService";
 
-const useSearchMovies = (searchInput, userId, orderByMethod, selectedGenres, selectedOrderASC) => {
-
+const useSearchMovies = (
+  searchInput,
+  userId,
+  orderByMethod,
+  selectedGenres,
+  selectedOrderASC
+) => {
   const [movieData, setMovieData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [firstSearch, setFirstSearch] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const [searchAttempted, setSearchAttempted] = useState(false);
   const [page, setPage] = useState(0);
@@ -15,27 +21,29 @@ const useSearchMovies = (searchInput, userId, orderByMethod, selectedGenres, sel
     if (selectedGenres.length === 0) {
       return movies;
     }
-    return movies.filter(movie =>
-      movie.genres.some(genre => selectedGenres.includes(genre.name))
+    return movies.filter((movie) =>
+      movie.genres.some((genre) => selectedGenres.includes(genre.name))
     );
   };
 
-  const handleSearch = async (newSearch = true) => {
+  const handleSearch = async (newSearch) => {
     let textInputValue = searchInput;
-    if (textInputValue === '' || textInputValue.trim().length === 0) {
+    if (textInputValue === "" || textInputValue.trim().length === 0) {
       return;
     }
 
-    const orderBy = orderByMethod || 'DATE';
+    setFirstSearch(newSearch);
+
+    const orderBy = orderByMethod || "DATE";
 
     setIsLoading(true);
     setSearchAttempted(true);
 
-    console.log("fetching page search", page)
+    console.log("fetching page search", page);
     try {
       const response = await movieService.searchMovies(
         textInputValue.trimStart(),
-        selectedOrderASC ? 'ASC' : 'DESC',
+        selectedOrderASC ? "ASC" : "DESC",
         orderBy,
         newSearch ? 1 : page,
         amountOfMoviesToGet,
@@ -44,21 +52,20 @@ const useSearchMovies = (searchInput, userId, orderByMethod, selectedGenres, sel
 
       if (response && response.movies) {
         const filteredMovies = filterGenres(response.movies, selectedGenres);
-        setMovieData(newSearch ? filteredMovies : [...movieData, ...filteredMovies]);
+        setMovieData(
+          newSearch ? filteredMovies : [...movieData, ...filteredMovies]
+        );
         setPage(newSearch ? 0 : page + 1);
         setHasMore(response.movies.length > 0);
-
       } else {
         setHasMore(false);
       }
-      
-
-
     } catch (error) {
       console.error("Error fetching movies: ", error);
       setHasMore(false);
     }
     setIsLoading(false);
+    setFirstSearch(false);
   };
 
   const handleLoadMore = () => {
@@ -67,8 +74,16 @@ const useSearchMovies = (searchInput, userId, orderByMethod, selectedGenres, sel
     }
   };
 
-
-  return { movieData, isLoading, hasMore, searchAttempted, page, handleSearch, handleLoadMore };
+  return {
+    movieData,
+    isLoading,
+    firstSearch,
+    hasMore,
+    searchAttempted,
+    page,
+    handleSearch,
+    handleLoadMore,
+  };
 };
 
 export default useSearchMovies;
--- frontend/src/screens/Search/Search.header.js
+++ frontend/src/screens/Search/Search.header.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { View, TextInput, TouchableOpacity } from 'react-native';
-import SearchIcon from '../../assets/images/search_btn_black.svg';
-import { styles } from './Search.styles';
+import React from "react";
+import { View, TextInput, TouchableOpacity } from "react-native";
+import SearchIcon from "../../assets/images/search_btn_black.svg";
+import { styles } from "./Search.styles";
 
-export default function SearchHeader({ inputRef, searchInput, setSearchInput, handleSearch }) {
+export default function SearchHeader({
+  inputRef,
+  searchInput,
+  setSearchInput,
+  handleSearch,
+}) {
   return (
     <View style={styles.headerContainer}>
       <View style={styles.searchIcon}>
         <TouchableOpacity
           style={styles.searchIcon.btn}
-          onPress={() => handleSearch(true)}>
+          onPress={() => handleSearch(true)}
+        >
           <SearchIcon width={28} height={28} />
         </TouchableOpacity>
       </View>
       <View style={styles.inputContainer}>
         <TextInput
           ref={inputRef}
-          onChangeText={text => setSearchInput(text)}
+          onChangeText={(text) => setSearchInput(text)}
           style={styles.inputText}
           placeholder="Buscar Película o Actor"
           onSubmitEditing={() => handleSearch(true)}
-          placeholderTextColor={'#303030'}
+          placeholderTextColor={"#303030"}
         />
       </View>
     </View>

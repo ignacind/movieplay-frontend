88922ca3db6254660d2c645d7bbb0bd0eb9120a0
Lucas <lucasvonelm@gmail.com>
2024-07-06 14:43:59 -0300

    data removed from autologin on logout

--- frontend/src/navigation/Navigation.js
+++ frontend/src/navigation/Navigation.js
@@ -1,52 +1,60 @@
-import React, { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
-import { View, StyleSheet, Pressable } from 'react-native';
-import { getTokens, getUserId } from '../services/storageService';
-import { login, logout } from '../redux/slices/authSlice';
-import ErrorScreen from '../screens/Error/ErrorScreen';
-import LoadingPage from '../components/LoadingPage';
-import { clearUserId, setUserId } from '../redux/slices/userSlice';
-import { AuthStack, MainStack } from './Stack';
-
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { View, StyleSheet, Pressable } from "react-native";
+import { getTokens, getUserId } from "../services/storageService";
+import { login, logout } from "../redux/slices/authSlice";
+import ErrorScreen from "../screens/Error/ErrorScreen";
+import LoadingPage from "../components/LoadingPage";
+import { clearUserId, setUserId } from "../redux/slices/userSlice";
+import { AuthStack, MainStack } from "./Stack";
 
 const Navigation = () => {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  const error = useSelector(state => state.error.error);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const error = useSelector((state) => state.error.error);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const checkTokens = async () => {
-  //     const { accessToken, refreshToken } = await getTokens();
-  //     const userId = await getUserId();
-  //     if (accessToken && refreshToken && userId) {
-  //       dispatch(login({ accessToken: accessToken, refreshToken: refreshToken, isAuthenticated: true}));
-  //       dispatch(setUserId(userId));
-  //     } else {
-  //       dispatch(logout());
-  //       dispatch(clearUserId());
-  //     }
-  //   };
+  useEffect(() => {
+    const checkTokens = async () => {
+      const { accessToken, refreshToken } = await getTokens();
+      const userId = await getUserId();
+      console.log("accessToken: " + accessToken);
+      console.log("refreshToken: " + refreshToken);
+      console.log("userId: " + userId);
+      if (accessToken && refreshToken && userId) {
+        dispatch(
+          login({
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            isAuthenticated: true,
+          })
+        );
+        dispatch(setUserId(userId));
+      } else {
+        dispatch(logout());
+        dispatch(clearUserId());
+      }
+    };
 
-  //   checkTokens();
-  // }, [dispatch]);
+    checkTokens();
+  }, [dispatch]);
 
   if (isAuthenticated === undefined) {
-    return (
-      <LoadingPage />
-    );
+    return <LoadingPage />;
   }
 
   return (
     <View style={styles.container}>
-      
       <NavigationContainer>
         {isAuthenticated ? <MainStack /> : <AuthStack />}
-        {error  && 
-        <ErrorScreen message={error?.message} iconName={error?.iconName} onRetry={error?.onRetry} />
-        }
+        {error && (
+          <ErrorScreen
+            message={error?.message}
+            iconName={error?.iconName}
+            onRetry={error?.onRetry}
+          />
+        )}
       </NavigationContainer>
-      
     </View>
   );
 };
